bee8b129c3e3591d0991aeb05565be14e67c0b2c
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-10 23:48:48 +0200

    annotation-add

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
-'''Complex types - string and int/float to tuple.
+'''Task 7
 '''
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:  # or (int | float)) -> float:
-    '''Return a tuple of a string and a float.
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
     '''
-    return (k, float( v**2))
+    return (k, float(v**2))
