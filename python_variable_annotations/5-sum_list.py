fc07b7a2b54dc8a61c0bb18eb410912ed64c40e1
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-10 22:51:53 +0200

    annotation-add

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''Complex types - list of floats.
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''Return the sum of a list of floats.
+    '''
+    return sum(input_list)
