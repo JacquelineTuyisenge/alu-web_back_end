c34be73b75098b148489144d3c1a1b84297418c7
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-10 22:44:38 +0200

    annotation-add

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''Basic annotation-concat.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concat two strings to return a string.
+    '''
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''Basic annotation-floor.
+'''
+
+
+def floor(n: float) -> int:
+    '''return a floor of a float.
+    '''
+    return int(n)
