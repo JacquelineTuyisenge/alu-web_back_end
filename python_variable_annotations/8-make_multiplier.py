3a83f08481c4cff2ba4ff45d725095d7532507db
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-10 23:58:07 +0200

    annotation-add

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
-'''Complex types - functions.
+'''Task 8
 '''
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable[[float], float]:  # or (int | float)) -> float:
-    '''make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier.
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Creates a multiplier function.
     '''
     return lambda x: x * multiplier
