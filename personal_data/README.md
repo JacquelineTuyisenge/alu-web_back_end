35802cf95a589996632403ef039b1bb1b9d4b4a4
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-25 21:46:58 +0200

    personal data

---
+++ personal_data/README.md
@@ -0,0 +1,1 @@
+readme file for **personal data** project in python
---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''Regex-ing.
+'''
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: list[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''The function should use a regex to replace occurrences of certain field values..
+    '''
+    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
+    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
