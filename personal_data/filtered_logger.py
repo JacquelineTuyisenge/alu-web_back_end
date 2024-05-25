dd988a033a700d7f4fc7e4ca8774b19083659842
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-25 21:56:30 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,4 +13,9 @@ def filter_datum(fields: List[str], redaction: str,
     '''should use a regex to replace occurrences of certain field values..
     '''
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
-    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
+    return re.sub(
+        pattern,
+        lambda m: m.group(0).split('=')[0] + '=' + redaction,
+        message
+    )
+
