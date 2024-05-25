e35d05c0ef72a863f5afbc2d6ae2b95fe083db95
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-25 21:52:23 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''The function should use a regex to replace occurrences of certain field values..
+    '''should use a regex to replace occurrences of certain field values..
     '''
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
     return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
