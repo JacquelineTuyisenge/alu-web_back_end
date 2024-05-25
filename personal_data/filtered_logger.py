e77a6f36b2dfc1b20dd9e12b8731f3829d6285b2
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-25 23:17:42 +0200

    personal data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -16,7 +16,6 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
 
-
     '''should use a regex to replace occurrences of certain field values..
     '''
 
@@ -46,6 +45,7 @@ class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
 
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -61,11 +61,7 @@ class RedactingFormatter(logging.Formatter):
                             super().format(record), self.SEPARATOR)
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-
-
-    '''Establish a connection to the database 
-    and return a connection object.
-    '''
+    '''Establish a connection to the database and return a connection object.'''
 
     user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
@@ -82,12 +78,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=database
     )
 def main() -> None:
-
-
     ''' main function'''
 
-    logging.basicConfig(
-        format='%(asctime)s %(name)s %(levelname)s %(message)s',
+    logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.INFO)
     logger = logging.getLogger('user_data')
@@ -100,10 +93,7 @@ def main() -> None:
     rows = cursor.fetchall()
 
     for row in rows:
-        message = f"name={row[0]};
-          email={row[1]}; phone={row[2]}; ssn={row[3]};
-            password={row[4]}; ip={row[5]}; last_login={row[6]};
-              user_agent={row[7]}"
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; ssn={row[3]}; password={row[4]}; ip={row[5]}; last_login={row[6]}; user_agent={row[7]}"
         filtered_message = filter_datum(fields, '***', message, '; ')
         logger.info(filtered_message)
 
