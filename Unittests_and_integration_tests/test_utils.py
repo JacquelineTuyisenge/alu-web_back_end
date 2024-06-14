22abf78f8f09a837bce82ce6399b8c7b12e505e9
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-06-14 12:04:19 +0200

    unit test in python

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -9,24 +9,26 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     '''testing utils'''
+
     @parameterized.expand([
-            ({"a": 1}, ("a",), 1),
-            ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-            '''testing access_nested_map'''
-            self.assertEqual(access_nested_map(nested_map, path), expected)
+        '''testing access_nested_map'''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
+        ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         '''testing access_nested_map for KeyError'''
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), str(path[-1]))
+        # Verify the exception message is as expected
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 if __name__ == '__main__':
     unittest.main()
