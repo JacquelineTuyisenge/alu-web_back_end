c3a11c0c68d9e59938ff12d19d444d6fa0470e36
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-06-14 12:12:39 +0200

    unit test in python

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 '''unittest'''
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,6 +30,27 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         # Verify the exception message is as expected
         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
+class TestGetJson(unittest.TestCase):
+    '''testing get_json'''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        '''get_json with mock'''
+        with patch('requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            response = get_json(test_url)
+
+            # Assert the mock was called once with the correct URL
+            mock_get.assert_called_once_with(test_url)
+            # Assert the response is the expected payload
+            self.assertEqual(response, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
