cb5528f46ddc8c0ddf1767fa3ebc362b2dd30300
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-06-29 22:50:20 +0200

    i18n

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Basic Flask app'''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -17,10 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def hello_world():
     '''Basic Flask app'''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
