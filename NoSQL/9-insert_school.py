cd1e80c52fd0283838cf639101cc1f5b73f411ed
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-07-06 21:52:53 +0200

    nosql database

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Python function that inserts a new document in a collection.
+'''Task 9: Insert a document in Python
 '''
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''Insert new document in a collection.
+    '''Inserts a new document in a collection.
     '''
     result = mongo_collection.insert_one(kwargs)
-    return result._id
+    return result.inserted_id
