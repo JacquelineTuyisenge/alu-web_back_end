c1b56dd3388bb5ca17b3f84a1596c4d0b95585dc
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-07-21 01:29:05 +0200

    node js basics

--- Node_JS_basic/full_Server/controllers/StudentsController.js
+++ Node_JS_basic/full_Server/controllers/StudentsController.js
@@ -7,11 +7,6 @@ class StudentsController {
         const students = [];
         // let count = 0;
         let msg;
-
-        // for (const key of Object.keys(fields)) {
-        //   count += fields[key].length;
-        // }
-
         // students.push(`Number of students: ${count}`);
         students.push('This is the list of our students');
 
